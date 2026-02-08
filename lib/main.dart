bbcc971db54140e922f9f1858b451a1e130b15dc
Anasahmed2005916 <karremahmed589@gmail.com>
2026-02-08 12:02:20 +0200

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:chat_app/pages/register_page.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(ScholarChat());
+  runApp(DevicePreview(builder: (context) => ScholarChat(), enabled: true));
 }
 
 class ScholarChat extends StatelessWidget {
